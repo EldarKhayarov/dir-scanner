cd9f80b50042b7cbcf76a3359afd102345c9fb7d
Eldar <hayaroff.eldar@yandex.ru>
2020-06-14 03:31:59 +0000

    Implemented directory scanner

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,37 +1,70 @@
 import sys
+import os
+import time
 
 
-IPS_FILE_FLAG = '-f'
-IPS_LIST_FLAG = '-ips'
-
-
-def get_ips_from_file(path: str):
-    # TODO: Реализовать функцию чтения айпишников из файла
-    pass
-
-
-def get_ips_from_args():
-    if IPS_FILE_FLAG in sys.argv:
-        try:
-            get_ips_from_file(sys.argv[sys.argv.index(IPS_FILE_FLAG) + 1])
-        except IndexError:
-            print('Отсутствует путь до файла с IP-адресами.')
-
-    elif IPS_LIST_FLAG in sys.argv:
-        return list([ip for ip in sys.argv[sys.argv.index(IPS_LIST_FLAG):]])
-    else:
-        raise KeyError('Не найдено специальных параметоров для списка ip.')
+class TCPSender:
+    def send(self, message):
+        # TODO: реализовать отправку по TCP
+        # print - временное решения для дебага
+        print(message)
 
 
 class Scanner:
-    ips: list
+    def __init__(self, path):
+        self.path = path
+        self.tcp_handler = TCPSender()
 
-    def __init__(self, ips, *args):
-        self.ips = ips
+    def _send_message(self, message):
+        self.tcp_handler.send(message)
+
+    @staticmethod
+    def _get_files_dict(path):
+        result = {
+            root + os.sep + file: os.stat(root + os.sep + file).st_mtime
+            for root, dirs, files in os.walk(path)
+            for file in files
+        }
+        return result
+
+    def run(self):
+        self._send_message(
+            'Запущен процесс сканирования директории ' + self.path
+        )
+        old_dict = self._get_files_dict(self.path)
+        while True:
+            new_dict = self._get_files_dict(self.path)
+
+            for file in new_dict:
+                if old_dict.get(file) is None:
+                    # Если в старом словаре файл найден не был,
+                    # значит этот файл был только что создан.
+                    self._send_message('Был создан файл: ' + file)
+                elif old_dict[file] != new_dict[file]:
+                    # Проверка времени последнего изменения.
+                    # Если значения не идентичны, значит произошла модификация.
+                    self._send_message('Был модифицирован файл: ' + file)
+
+            # Отдельно производится проверка на удаление файлов.
+            for file in old_dict:
+                if file not in new_dict:
+                    self._send_message('Был удалён файл: ' + file)
+
+            # Переопределение переменной старго словаря.
+            old_dict = new_dict
+            time.sleep(0.01)
 
 
 def direct_run():
-    scanner = Scanner(get_ips_from_args())
+    try:
+        # Для успешной работы необходимо добавить в качестве единственного
+        # аргумента путь целевой директории.
+        path = sys.argv[1]
+    except IndexError:
+        print('Необходимо добаить путь до сканируемой папки вторым аргументом.')
+    else:
+        scanner = Scanner(path)
+        scanner.run()
 
 
 if __name__ == '__main__':
